da45ca8e737ff699d105ef42fdd1fa84323fa87f
Rania Raafat <raniaraafat421@gmail.com>
2026-07-22 15:58:18 +0300

    Add Edits

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,10 +1,20 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_SMTP_USER,
+    pass: process.env.BREVO_SMTP_PASS,
+  },
+});
 
 export const sendEmail = async (to, subject, html) => {
-  const resend = new Resend(process.env.RESEND_API_KEY);
-
-  return await resend.emails.send({
-    from: "onboarding@resend.dev",
+  return await transporter.sendMail({
+    from: `"RQMMYAT" <raniaraafat421@gmail.com>`,
     to,
     subject,
     html,
